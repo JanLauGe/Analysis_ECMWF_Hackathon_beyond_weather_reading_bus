ab9bdad5c577c1c40512a3686c795ab31808e588
Laurens Geffert <laurensgeffert@gmail.com>
2017-03-04 22:22:56 +0000

    added special events and model prototype

--- model_prototype.R
+++ model_prototype.R
@@ -6,9 +6,16 @@ library(forcats)
 library(caret)
 library(stringr)
 
+setwd('C:/Users/Laurens/Dropbox/projects/ecmwf/code/ReadingBus/Data/')
 claims <- read_csv('busClaims.csv')
+apiKey <- readline('Enter API key:')
 
 # Data Cleaning --------------------------------------------------------
+
+# Get useful route names
+routes <- c('2','3','4','5','6','7','9','11','12','13','14','15','16','17',
+  '19a','19b','19c','21','22','23','24','25','26','27','28','33','500') %>%
+  factor
 
 claims %<>%
   mutate(
@@ -34,20 +41,15 @@ claims %<>%
         `6` = '6A',
         `27` = '29')) %>%
   # Create proper date field
-  mutate(dates = `Accident Date` %>% as.Date(format = '%d/%m/%y'))
-
-# Get useful route names
-routes <- c('2','3','4','5','6','7','9','11','12','13','14','15','16','17',
-  '19a','19b','19c','21','22','23','24','25','26','27','28','33','500') %>%
-  factor
-
-claims %<>% 
+  mutate(dates = `Accident Date` %>% as.Date(format = '%d/%m/%y')) %>%
   # Only claims in 2015 and 2016
-  filter(`Year of Account` > 2014) %>%
+  filter(year(dates) > 2014 & year(dates) < 2017) %>%
   # Only valid routes
   filter(route %in% routes) %>%
   # Make bad flag
-  mutate(claimed = 1)
+  mutate(claimed = 1) %>% 
+  select(one_of('dates', 'route', 'claimed'))
+
 
 # Create model data frame ----------------------------------------------
 modeldata <- data_frame(dates = seq(ymd('2015-01-01'), ymd('2016-12-31'), by = 'days')) %>%
@@ -58,11 +60,58 @@ modeldata <- data_frame(dates = seq(ymd('2015-01-01'), ymd('2016-12-31'), by = '
   # Set claims to zero
   mutate(claim = 0) %>%
   # Merge claims data
-  full_join(claims %>% select(dates, route, claimed), by = c('dates', 'route')) %>%
+  full_join(claims, by = c('dates', 'route')) %>%
   # Get number of claims per day per route
   group_by(dates, route) %>%
   summarise(claims = sum(claim, na.rm = TRUE) + sum(claimed, na.rm = TRUE)) %>%
-  mutate(claims)
+  mutate(claims = ifelse(claims > 0, 'yes', 'no') %>% as.factor())
+
+# Plot bad flag over time
+modeldata %>%
+  filter(claims == 'yes') %>%
+  ggplot(aes(x = dates)) +
+  geom_histogram(binwidth = 1)
+
+
+# Run the model ---------------------------------------------------------
+set.seed(1717)
+kfolds <- createFolds(modeldata$claims, k = 3)
+
+data_train <- modeldata[-kfolds[[3]],]
+data_test <- modeldata[kfolds[[3]],]
+  
+
+mod_eval <- trainControl(summaryFunction = madSummary)
+mod_tune <- expand.grid()
+
+
+
+train(
+  x = modeldata,
+  )
+
+
+# Train glmnet model
+mod_tune <- expand.grid(
+  alpha = c(0.1, 0.5, 1), #c(0.1, 0.2, 0.3, 0.5, 0.7, 1), 
+  lambda = c(0.0001, 0.001, 0.01)) #c(0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1))
+mod_eval <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 3,
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary)
+mod_train <- train(
+  claims ~ 1 + .,
+  data = modeldata,
+  method = "glmnet",
+  family = "binomial",
+  metric = "ROC",
+  tuneGrid = mod_tune,
+  trControl = mod_eval)
+
+
+
 
 
 
